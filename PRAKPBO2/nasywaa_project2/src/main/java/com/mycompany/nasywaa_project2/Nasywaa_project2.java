a4496f310cea3ff161adc66299954cfd7a6d5f93
nasywaajihaan <nasywaajihaan16@gmail.com>
2024-09-01 12:23:34 +0700

    Add files via upload

---
+++ PRAKPBO2/nasywaa_project2/src/main/java/com/mycompany/nasywaa_project2/Nasywaa_project2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.nasywaa_project2;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Nasywaa_project2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Nasywaa!");
+        System.out.println("2340506078!");
+        System.out.println("magelang!");
+        System.out.println("0819284758493!");
+    }
+}
