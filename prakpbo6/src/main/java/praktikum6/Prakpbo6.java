0df6dc99594118e183192ad29b63c2172493e443
nasywaajihaan <nasywaajihaan16@gmail.com>
2024-10-04 18:51:34 +0700

    Add files via upload

---
+++ prakpbo6/src/main/java/praktikum6/Prakpbo6.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package praktikum6;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Prakpbo6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
